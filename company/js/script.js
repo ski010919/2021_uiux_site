f1c75258acfb746a9f2ec0d6128d83077593cbe7
ski010919 <ski010919@naver.com>
2021-09-13 15:11:56 +0900

    company_0913

--- company/js/script.js
+++ company/js/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-    $(".slider").bxSlider();
+    const slider1 = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });
+    $(".prevBtn").click(function(){
+        slider1.goToPrevSlide();
+    });
+
+    // $(".toggle").on("이벤트이름",리스너);  < 이벤트
+    // $(".toggle").on("click",function(){});  <이벤트 예시
+    // $(".toggle").click(function(){});   <축약형
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); // value toggle
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            // $(".toggle").addClass("active");
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            // $(".toggle").removeClass("active");
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
 });
