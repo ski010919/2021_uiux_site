6370fded42c973ab9e254a528e602fe3d27a0135
ski010919 <ski010919@naver.com>
2021-09-14 18:18:52 +0900

    kensington_v4

--- 04 kensington/js/script.js
+++ 04 kensington/js/script.js
@@ -1,46 +1,45 @@
-// $(document).ready(function(){
-//     const slider1 = $(".slider").bxSlider({
-//         controls:false
-//     });
-//     $(".nextBtn").click(function(){
-//         slider1.goToNextSlide();
-//     });
-//     $(".prevBtn").click(function(){
-//         slider1.goToPrevSlide();
-//     });
-
-//     // $(".toggle").on("이벤트이름",리스너);  < 이벤트
-//     // $(".toggle").on("click",function(){});  <이벤트 예시
-//     // $(".toggle").click(function(){});   <축약형
-//     $(".toggle").click(function(){
-//         const toggle = $(".toggle").attr("class"); // value toggle
-//         console.log(toggle);
-
-//         if( toggle == "toggle"){
-//             // $(".toggle").addClass("active");
-//             document.getElementsByClassName("toggle")[0].classList.add("active");
-//             $(".gnb").addClass("view");
-//         }else{
-//             // $(".toggle").removeClass("active");
-//             document.getElementsByClassName("toggle")[0].classList.remove("active");
-//             $(".gnb").removeClass("view");
-//         }
-//     });
-
-//     $(window).resize(function(){
-//         $(".toggle").removeClass("active");
-//         $(".gnb").removeClass("view");
-//     });
-// });
-
 $(document).ready(function(){
+    const slider1 = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });
+    $(".prevBtn").click(function(){
+        slider1.goToPrevSlide();
+    });
+
+    const slider2 = $(".slider02").bxSlider({
+        controls:false,
+        pager:false,
+    });
+    $(".pager_next").click(function(){
+        slider2.goToNextSlide();
+    });
+    $(".pager_prev").click(function(){
+        slider2.goToPrevSlide();
+    });
+
     $(".toggle").click(function(){
         const toggle = $(".toggle").attr("class");
 
         if(toggle == "toggle"){
             $(".toggle").addClass("active");
+            $(".gnb").addClass("view");
         }else{
             $(".toggle").removeClass("active");
+            $(".gnb").removeClass("view");
         }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
+
+    $(".youtube").colorbox({
+        iframe:true,
+        innerWidth:700,
+        innerHeight:394,
     });
 });
