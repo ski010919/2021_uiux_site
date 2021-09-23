3da790ed4778a12713323810cb9f74dd1d819467
ski010919 <ski010919@naver.com>
2021-09-23 18:01:24 +0900

    kensington_v8

--- 04kensington/js/script.js
+++ 04kensington/js/script.js
@@ -67,5 +67,25 @@ $(document).ready(function(){
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-      });
+    });
+
+    const tabList = document.querySelectorAll(".tab li")
+    const conHome = document.querySelectorAll(".con_home")
+    const conTitle = document.querySelectorAll(".conTitle")
+    const conImg = document.querySelectorAll(".conBottom img")
+
+    for(let j=0;j<tabList.length;j++){
+        tabList[j].onclick = function(){
+            for(let i=0;i<tabList.length;i++){
+                tabList[i].classList.remove("on");
+                conHome[i].classList.remove("on");
+                conTitle[i].classList.remove("on");
+                conImg[i].classList.remove("on");
+            }
+            tabList[j].classList.add("on");
+            conHome[j].classList.add("on");
+            conTitle[j].classList.add("on");
+            conImg[j].classList.add("on");
+        }
+    }
 });
