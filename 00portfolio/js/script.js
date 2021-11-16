c79cf9cbb0aecb2d5455600a9a0f22b5334aefa2
ski010919 <ski010919@naver.com>
2021-11-16 18:19:48 +0900

    portfolio

---
+++ 00portfolio/js/script.js
@@ -0,0 +1,7 @@
+$("#fullpage").fullpage({
+    anchors:["m1st","m2st","m3st","m4st"],
+    navigation:true,
+    navigationPosition: 'fp-right',
+    navigationTooltips: ['main', 'First', 'Second','Third','footer'],
+    responsiveWidth: 768,
+});
